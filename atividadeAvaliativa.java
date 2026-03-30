82e5ac41543f946462d6768c49e6f3ae9265145f
guiDaros <guivdaros@gmail.com>
2026-03-30 20:50:21 -0300

    fst commit. Inputs setted.

---
+++ atividadeAvaliativa.java
@@ -0,0 +1,16 @@
+void main(){
+    String pesoStr = IO.readln("Digite seu Peso: ");
+    double peso = Double.parseDouble(pesoStr);
+    String alturaStr = IO.readln("Digite sua Altura: ");
+    double altura = Double.parseDouble(alturaStr);
+    char genero = IO.readln("Digite o Genero: M -> Masculino, F -> Feminino, N -> Nao identifico com nenhum ").charAt(0);
+
+    switch (genero) {
+        case 'm', 'M':
+            
+            break;
+    
+        default:
+            break;
+    }
+}
